972ab6b4e935de3b3a44b83436ffc7fea6d6f042
Hamed Niroomand <dev.hamedniroomand@gmail.com>
2022-07-10 17:58:09 +0430

    feat: create and use number utility for random numbers

--- src/core/models/ProfileModel.js
+++ src/core/models/ProfileModel.js
@@ -1,6 +1,8 @@
+import { randomNumber } from "../utils/numberUtils";
+
 export class ProfileModel {
     constructor({ name, email, description, specializations, likes = 0, }) {
-        this.id = Math.floor(Math.random() * 9999999);
+        this.id = randomNumber();
         this.name = name;
         this.email = email;
         this.description = description;
