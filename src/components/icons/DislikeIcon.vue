4b21ba43fed73645b38016b8589615940e6fba31
Hamed Niroomand <dev.hamedniroomand@gmail.com>
2022-07-10 20:05:06 +0430

    feat: create icon mixin and add like and dislike icon components

---
+++ src/components/icons/DislikeIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <LikeIcon class="rotate-180" v-bind="$props" />
+</template>
+
+<script>
+import iconMixin from "../../core/mixins/iconMixin";
+import LikeIcon from "./LikeIcon.vue";
+
+export default {
+  mixins: [iconMixin],
+  components: { LikeIcon },
+};
+</script>
---
+++ src/core/mixins/iconMixin.js
@@ -0,0 +1,12 @@
+export default {
+  props: {
+    fill: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: [Number, Boolean],
+      default: 24,
+    },
+  },
+};
