6021ff3764ef3e4f3c58a5beda7fc10e1c1a1dd0
Hamed Niroomand <dev.hamedniroomand@gmail.com>
2022-07-10 20:13:35 +0430

    feat: add isLiked and isDisliked to profile model

--- src/models/ProfileModel.js
+++ src/models/ProfileModel.js
@@ -1,12 +1,14 @@
 import { randomNumber } from "../core/utils/numberUtils";
 
 export class ProfileModel {
-  constructor({ name, email, description, specializations, likes = 0 }) {
+  constructor({ name, email, description, specializations, likes = 0, isLiked = false, isDisliked = false }) {
     this.id = randomNumber();
     this.name = name;
     this.email = email;
     this.description = description;
     this.specializations = specializations;
     this.likes = likes;
+    this.isLiked = isLiked;
+    this.isDisliked = isDisliked;
   }
 }
